08d376892442621975444df6dc6a95ccbd4f3a0f
jaehoonchoi <eeoos8840@gmail.com>
2025-06-09 12:01:38 +0900

    feat(article): 게시글 생성 controller 구현

---
+++ article/src/main/java/com/codesquad/article/article/api/ArticleController.java
@@ -0,0 +1,31 @@
+package com.codesquad.article.article.api;
+
+import java.net.URI;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import jakarta.servlet.http.HttpSession;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+@RequestMapping("/api")
+@RestController
+public class ArticleController {
+
+	private final ArticleService articleService;
+	private static final Long FIRST_USER_ID = 1L;
+	@PostMapping("/v1/artices")
+	public ResponseEntity<ApiResponse<ArticleDto.CreateResponse>> createArticle(
+		@Valid @RequestBody ArticleDto.CreateRequest request) {
+		ArticleDto.CreateResponse response = articleService.createArticle(request, FIRST_USER_ID);
+		URI uri = new URI("/api/v1/artices" + response.id());
+		return ResponseEntity.created(uri).body(ApiResponse.success(
+			response
+		));
+	}
+}
