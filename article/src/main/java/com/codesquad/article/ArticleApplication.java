4e8bf1a51cdbbd8f0b243a9216f1c0ede72339cc
jaehoonchoi <eeoos8840@gmail.com>
2025-06-09 15:39:29 +0900

    fix: @EnableJpaAuditing를 넣어서 updatedAt, createdAt에 값이 들어가도록 설정

--- article/src/main/java/com/codesquad/article/ArticleApplication.java
+++ article/src/main/java/com/codesquad/article/ArticleApplication.java
@@ -2,8 +2,10 @@ package com.codesquad.article;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class ArticleApplication {
 
 	public static void main(String[] args) {
