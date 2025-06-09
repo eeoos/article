6ee97ac6f6551c0c76aeda3277285038e1721d49
jaehoonchoi <eeoos8840@gmail.com>
2025-06-09 13:09:10 +0900

    feat(user): 회원가입 controller 로직 구현

---
+++ article/src/main/java/com/codesquad/article/user/api/UserController.java
@@ -0,0 +1,30 @@
+package com.codesquad.article.user.api;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.codesquad.article.common.dto.ApiResponse;
+
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+
+@RequestMapping("/api")
+@RequiredArgsConstructor
+@RestController
+public class UserController {
+
+	private final UserService userService;
+
+	@PostMapping("/v1/signup")
+	public ResponseEntity<ApiResponse<UserDto.CreateResponse>> signUp(
+		@Valid @RequestBody UserDto.CreateRequest request) {
+
+		UserDto.CreateResponse response = userService.signUp(request);
+		return ResponseEntity.ok(ApiResponse.success(
+			response
+		));
+	}
+}
