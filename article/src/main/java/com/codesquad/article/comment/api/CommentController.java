161708662d5bf4f5b0e94a21a7615fd36cd9fd39
jaehoonchoi <eeoos8840@gmail.com>
2025-06-09 12:28:06 +0900

    feat(comment): 댓글 생성 controller 로직 작성

---
+++ article/src/main/java/com/codesquad/article/comment/api/CommentController.java
@@ -0,0 +1,37 @@
+package com.codesquad.article.comment.api;
+
+import java.net.URI;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.codesquad.article.common.dto.ApiResponse;
+
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+
+@RequestMapping("/api")
+@RequiredArgsConstructor
+@RestController
+public class CommentController {
+
+	private final CommentService commentService;
+	private static final Long FIRST_USER_ID = 1L;
+
+	@PostMapping("/v1/issues/{issueId}/comments")
+	public ResponseEntity<ApiResponse<CommentDto.CreateResponse>> createComment(
+		@PathVariable Long issueId,
+		@Valid @RequestBody CommentDto.CreateRequest request) {
+
+		CommentDto.CreateResponse response= commentService.createComment(issueId, request, FIRST_USER_ID);
+		URI uri = new URI("/api/v1/issues/" + response.issueId() + "comments/" + response.commentId());
+		return ResponseEntity.created(uri).body(ApiResponse.success(
+				response
+			)
+		);
+	}
+}
