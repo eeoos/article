acb19a972423c31804a74a221e181aa4ed8e46a4
jaehoonchoi <eeoos8840@gmail.com>
2025-06-09 12:42:35 +0900

    feat(article): 게시글 삭제 controller 로직 구현

--- article/src/main/java/com/codesquad/article/article/api/ArticleController.java
+++ article/src/main/java/com/codesquad/article/article/api/ArticleController.java
@@ -4,6 +4,8 @@ import java.net.URI;
 import java.net.URISyntaxException;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -23,13 +25,20 @@ public class ArticleController {
 
 	private final ArticleService articleService;
 	private static final Long FIRST_USER_ID = 1L;
+
 	@PostMapping("/v1/articles")
 	public ResponseEntity<ApiResponse<ArticleDto.CreateResponse>> createArticle(
 		@Valid @RequestBody ArticleDto.CreateRequest request) throws URISyntaxException {
 		ArticleDto.CreateResponse response = articleService.createArticle(request, FIRST_USER_ID);
-		URI uri = new URI("/api/v1/artices" + response.id());
+		URI uri = new URI("/api/v1/articles" + response.id());
 		return ResponseEntity.created(uri).body(ApiResponse.success(
 			response
 		));
+	}
+
+	@DeleteMapping("/v1/articles/{articleId}")
+	public ResponseEntity<Void> deleteArticle(@PathVariable Long articleId) {
+		articleService.deleteArticle(articleId, FIRST_USER_ID);
+		return ResponseEntity.noContent().build();
 	}
 }
